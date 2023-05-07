b490cc9515fdbe0ba24d7487e5ba1b6ac0bd77ab
IemurIord14 <132770783+IemurIord14@users.noreply.github.com>
2023-05-07 15:40:44 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,218 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "forestMonkeyWalkLeft1":
+            case "anim3":return [img`
+. . . . f f f f f . . . . . . . 
+. . . f f f f f f f . . . . . . 
+. . f d d d d f f f f . . . . . 
+. f d f d d f d f f f f . . . . 
+. f d f d d f d f f d d f . . . 
+f d d d d d d d f f b d c . . . 
+f d d d d c d d 5 5 b d c . f f 
+f c c c c d d d 5 5 f c . f 5 f 
+. f d d d d d 5 5 f f . . f 5 f 
+. . f f f f f 5 5 5 5 f . f 5 f 
+. . . . f 5 5 5 5 5 5 5 f f 5 f 
+. . . f 5 f f 5 f 5 5 5 5 f f . 
+. . . f 5 f f 5 f 5 5 5 5 f . . 
+. . . f d b f d b f f 5 f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`];
+            case "forestMonkeyWalkLeft":
+            case "anim1":return [img`
+. . . . f f f f f . . . . . . . 
+. . . f f f f f f f . . . . . . 
+. . f d d d d f f f f . . . . . 
+. f d f d d f d f f f f . . . . 
+. f d f d d f d f f d d f . . . 
+f d d d d d d d f f b d c . . . 
+f d d d d c d d 5 5 b d c . f f 
+f c c c c d d d 5 5 f c . f 5 f 
+. f d d d d d 5 5 f f . . f 5 f 
+. . f f f f f 5 5 5 5 f . f 5 f 
+. . . . f 5 5 5 5 5 5 5 f f 5 f 
+. . . f 5 f f 5 f 5 5 5 5 f f . 
+. . . f 5 f f 5 f 5 5 5 5 f . . 
+. . . f d b f d b f f 5 f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`, img`
+. . . . f f f f f . . . . . . . 
+. . . f f f f f f f . . . . . . 
+. . f d d d d f f f f . . . . . 
+. f d f d d f d f f f . . . . . 
+. f d f d d f d f f f f . . . . 
+f d d d d d d d f f d d f . . . 
+f d d d d c d d f f b d c . . . 
+f c c c c d d 5 5 5 b d c . f f 
+. f d d d d 5 5 5 f f c . f 5 f 
+. f f f f f f 5 5 5 5 f . f 5 f 
+. f f f f 5 5 5 5 5 5 5 f f 5 f 
+f d d f d d f 5 f 5 5 5 5 f f . 
+f d b f d b f 5 f 5 5 5 5 f . . 
+f f f f f f f f f f f f 5 f . . 
+. . . . . . . . . f c d d f . . 
+. . . . . . . . . . f f f f . . 
+`, img`
+. . . . f f f f f . . . . . . . 
+. . . f f f f f f f . . . . . . 
+. . f d d d d f f f f f . . . . 
+. f d d d d d d f f d d f . . . 
+. f d f d d f d f f b d c . . . 
+f d d f d d f d 5 5 b d c . f f 
+f d d d d d d d 5 5 f c . f 5 f 
+f d d d d c d 5 5 5 f . . f 5 f 
+. f c c c d 5 5 5 f f . . f 5 f 
+. . f f f f f 5 5 5 5 f . f 5 f 
+. . . . f 5 5 5 5 5 5 5 f f f . 
+. . f f 5 f 5 5 f 5 5 5 5 f . . 
+. f 5 f f 5 5 f f f 5 5 5 f . . 
+f d d b d d c f f f f f f b f . 
+f d d c d d d f . . f c d d f . 
+. f f f f f f f . . . f f f . . 
+`, img`
+. . . . f f f f f . . . . . . . 
+. . . f f f f f f f f f . . . . 
+. . f d d d f f f f d d f . . . 
+. f d d d d d f f f b d c . . . 
+. f d d d d d d 5 5 b d c . . . 
+f d d f d d f d 5 5 f c . f f . 
+f d d f d d f d 5 5 f . . f 5 f 
+f d d d d d d d 5 5 f . . f 5 f 
+. f d d d c d 5 5 f f . . f 5 f 
+. . f f f d 5 5 5 5 5 f . f 5 f 
+. . . . f 5 5 5 5 5 5 5 f f f . 
+. . . . f f 5 5 5 5 5 b f f . . 
+. . . f 5 f f 5 5 c d d f f . . 
+. . f d d b d d c f f f . . . . 
+. . f d d c d d d f f . . . . . 
+. . . f f f f f f f . . . . . . 
+`, img`
+. . . . f f f f f . . . . . . . 
+. . . f f f f f f f . . . . . . 
+. . f d d d d f f f f . . . . . 
+. f d f d d f d f f f f . . . . 
+. f d f d d f d f f d d f . . . 
+f d d d d d d d f f b d c . . . 
+f d d d d c d d 5 5 b d c . . . 
+f c c c c d d 5 5 5 f c . . . . 
+. f d d d d 5 5 5 f f . . . . . 
+. . f f f f f 5 5 5 5 f . . . . 
+. . . . f f 5 5 5 5 5 5 f . f f 
+. . . f 5 5 f 5 5 f 5 5 f . 5 f 
+. . f 5 5 f 5 5 f 5 5 5 f . 5 f 
+. f b d f d b f b b f 5 f f 5 f 
+. f d d f d d f d d b 5 f f f f 
+. . f f f f f f f f f f f f f . 
+`];
+            case "forestMonkeyWalkLeft0":
+            case "anim2":return [img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f f f f f f f . . . 
+. . . . . f f f f d d d d f . . 
+. . . . f f f f d f d d f d f . 
+. . . f d d f f d f d d f d f . 
+. . . c d b f f d d d d d d d f 
+f f . c d b 5 5 d d c d d d d f 
+f 5 f . c f 5 5 d d d c c c c f 
+f 5 f . . f f 5 5 d d d d d f . 
+f 5 f . f 5 5 5 5 f f f f f . . 
+f 5 f f 5 5 5 5 5 5 5 f . . . . 
+. f f 5 5 5 5 f 5 f f 5 f . . . 
+. . f 5 5 5 5 f 5 f f 5 f . . . 
+. . . f 5 f f b d f b d f . . . 
+. . . f d b b d d c d d f . . . 
+. . . f f f f f f f f f . . . . 
+`, img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f f f f f f f . . . 
+. . . . . f f f f d d d d f . . 
+. . . . . f f f d f d d f d f . 
+. . . . f f f f d f d d f d f . 
+. . . f d d f f d d d d d d d f 
+. . . c d b f f d d c d d d d f 
+f f . c d b 5 5 5 d d c c c c f 
+f 5 f . c f f 5 5 5 d d d d f . 
+f 5 f . f 5 5 5 5 f f f f f f . 
+f 5 f f 5 5 5 5 5 5 5 f f f f . 
+. f f 5 5 5 5 f 5 f d d f d d f 
+. . f 5 5 5 5 f 5 f b d f b d f 
+. . f 5 f f f f f f f f f f f f 
+. . f d d c f . . . . . . . . . 
+. . f f f f . . . . . . . . . . 
+`, img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f f f f f f f . . . 
+. . . . f f f f f d d d d f . . 
+. . . f d d f f d d d d d d f . 
+. . . c d b f f d f d d f d f . 
+f f . c d b 5 5 d f d d f d d f 
+f 5 f . c f 5 5 d d d d d d d f 
+f 5 f . . f 5 5 5 d c d d d d f 
+f 5 f . . f f 5 5 5 d c c c f . 
+f 5 f . f 5 5 5 5 f f f f f . . 
+. f f f 5 5 5 5 5 5 5 f . . . . 
+. . f 5 5 5 5 f 5 5 f 5 f f . . 
+. . f 5 5 5 f f f 5 5 f f 5 f . 
+. f b f f f f f f c d d b d d f 
+. f d d c f . . f d d d c d d f 
+. . f f f . . . f f f f f f f . 
+`, img`
+. . . . . . . f f f f f . . . . 
+. . . . f f f f f f f f f . . . 
+. . . f d d f f f f d d d f . . 
+. . . c d b f f f d d d d d f . 
+. . . c d b 5 5 d d d d d d f . 
+. f f . c f 5 5 d f d d f d d f 
+f 5 f . . f 5 5 d f d d f d d f 
+f 5 f . . f 5 5 d d d d d d d f 
+f 5 f . . f f 5 5 d c d d d f . 
+f 5 f . f 5 5 5 5 5 d f f f . . 
+. f f f 5 5 5 5 5 5 5 f . . . . 
+. . f f b 5 5 5 5 5 f f . . . . 
+. . f f d d c 5 5 f f 5 f . . . 
+. . . . f f f c d d b d d f . . 
+. . . . . f f d d d c d d f . . 
+. . . . . . f f f f f f f . . . 
+`, img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f f f f f f f . . . 
+. . . . . f f f f d d d d f . . 
+. . . . f f f f d f d d f d f . 
+. . . f d d f f d f d d f d f . 
+. . . c d b f f d d d d d d d f 
+. . . c d b 5 5 d d c d d d d f 
+. . . . c f 5 5 5 d d c c c c f 
+. . . . . f f 5 5 5 d d d d f . 
+. . . . f 5 5 5 5 f f f f f . . 
+f f . f 5 5 5 5 5 5 f f . . . . 
+f 5 . f 5 5 f 5 5 f 5 5 f . . . 
+f 5 . f 5 5 5 f 5 5 f 5 5 f . . 
+f 5 f f 5 f b b f b d f d b f . 
+f f f f 5 b d d f d d f d d f . 
+. f f f f f f f f f f f f f . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,74 @@
-
+function LemurLad () {
+    lemurlad = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    animation.runImageAnimation(
+    lemurlad,
+    assets.animation`forestMonkeyWalkLeft1`,
+    100,
+    true
+    )
+    scene.cameraFollowSprite(lemurlad)
+}
+function background () {
+    tiles.setCurrentTilemap(tilemap`level`)
+    tiles.placeOnTile(lemurlad, tiles.getTileLocation(3, 13))
+}
+function functions () {
+    extras()
+    LemurLad()
+    background()
+}
+function extras () {
+    pixelstometers = 30
+    gravity = 9.81 * pixelstometers
+}
+let gravity = 0
+let pixelstometers = 0
+let lemurlad: Sprite = null
+functions()
+game.onUpdate(function () {
+    if (lemurlad.isHittingTile(CollisionDirection.Bottom) && controller.A.isPressed()) {
+        lemurlad.setVelocity(0, -20000 * pixelstometers)
+    }
+})
+forever(function () {
+    if (controller.left.isPressed()) {
+        animation.runImageAnimation(
+        lemurlad,
+        assets.animation`forestMonkeyWalkLeft`,
+        100,
+        true
+        )
+        lemurlad.x += -2
+    }
+    if (controller.right.isPressed()) {
+        animation.runImageAnimation(
+        lemurlad,
+        assets.animation`forestMonkeyWalkLeft0`,
+        100,
+        true
+        )
+        lemurlad.x += 2
+    }
+})
+game.onUpdateInterval(100, function () {
+    if (!(lemurlad.isHittingTile(CollisionDirection.Bottom))) {
+        lemurlad.setVelocity(0, 45)
+    }
+})
